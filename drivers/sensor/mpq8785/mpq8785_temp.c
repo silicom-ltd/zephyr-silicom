3b9035ab7187f89da9af499e9a03526604b0aaa6
Jeff Daly <jeffd@silicom-usa.com>
2025-12-10 21:23:42 -0500

    sensors/mpq8785: Return temperature in C/mC.
    Temperature sensors return values in C/mC in val1/val2.
    
    Signed-off-by: Jeff Daly <jeffd@silicom-usa.com>

--- drivers/sensor/mpq8785/mpq8785_temp.c
+++ drivers/sensor/mpq8785/mpq8785_temp.c
@@ -81,8 +81,8 @@ static int mpq8785_temp_channel_get(const struct device *dev, enum sensor_channe
 		return -ENOTSUP;
 	}
 
-	val->val1 = data->temperature;
-	val->val2 = 0;
+	val->val1 = (data->temperature / 1000);
+	val->val2 = (data->temperature % 1000) * 1000;
 	return 0;
 }
 
